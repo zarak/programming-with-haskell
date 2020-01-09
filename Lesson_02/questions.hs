191af329c1b3f399beb9522f0c751f7785c3b7db
Zarak <zarak@hotmail.ca>
2020-01-09 23:06:22 +0500

    Complete question 2.3 - closes #1

--- Lesson_02/questions.hs
+++ Lesson_02/questions.hs
@@ -1,7 +1,23 @@
--- Q2.2
 main :: IO ()
 
+-- Q2.2
 inc n = n + 1
+
+double n = 2 * n
+
+square n = n * n
+
+-- Q2.3
+evenOrOdd n = 
+    if even n 
+    then n - 2
+    else 3 * n + 1
 
 main = do
     print (inc 10)
+    print (double 10)
+    print (square 10)
+    print ("Question 2.3 even")
+    print (evenOrOdd 10)
+    print ("Question 2.3 odd")
+    print (evenOrOdd 11)
